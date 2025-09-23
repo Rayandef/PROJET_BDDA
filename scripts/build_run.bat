7c4f9f6b3b9cb34e9bbaceba052b76be230650f4
RonanLall <ronanlall@gmail.com>
2025-09-23 19:28:08 +0200

    Correction build_run.bat

--- scripts/build_run.bat
+++ scripts/build_run.bat
@@ -1,5 +1,6 @@
-echo off
+@echo off
 if not exist bin mkdir bin
-javac -d bin src\bdda\*.java
-javac -d bin src\bdda\**\*.java
-java -cp bin dbba.Main
+javac -d bin -cp src src\bdda\Main.java 
+javac -d bin src\bdda\config\DBConfig.java 
+javac -d bin src\bdda\util\IO.java
+java -cp bin;config bdda.Main
