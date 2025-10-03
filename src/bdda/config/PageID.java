063db10d4077a79d8705a058d7ba27217a0486ca
jordanalaicrisosto-cyber <jordanalaicrisosto@gmail.com>
2025-10-03 14:24:58 +0200

    Classes à compléter

---
+++ src/bdda/config/PageID.java
@@ -0,0 +1,10 @@
+package bdda.config;
+
+/**Classe représentant la classification des pages dans chaque fichier 
+ * @author !Jordan
+ * @author !1.0
+*/
+public class PageID {
+    private int FileIdx;
+    private int PageIdx;
+}
