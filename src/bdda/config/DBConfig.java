c4867c0db5b41e3874426fd1d81adadd16fa3209
RonanLall <ronanlall@gmail.com>
2025-10-03 15:39:34 +0200

    Amélioration des test DBConfig

--- src/bdda/config/DBConfig.java
+++ src/bdda/config/DBConfig.java
@@ -2,16 +2,33 @@ package bdda.config;
 
 import java.io.FileInputStream;
 import java.util.Properties;
-
+    /** Classe permettant le chargement de la base de donnée
+    */
 public class DBConfig {
+    /**dbpath -> l'emplacement de la base de données */
     private String dbpath;
     private int pageSize;
     private int dm_maxfilecount;
 
-    public DBConfig(String dbpath, int pageSize, int dm_maxfilecount) {
+    public DBConfig(String dbpath, int pageSize, int dm_maxfilecount) throws Exception{
         this.dbpath = dbpath;
+        if(dbpath!="./DB/binData"){
+            throw new Exception("dbpath incorrect");
+        }
+        if(pageSize<=0){
+            throw new Exception("On ne peut pas avoir une page de taille nulle");
+        }
         this.pageSize = pageSize;
+        if(dm_maxfilecount<=0){
+            throw new Exception("On ne peut pas avec 0 fichiers max");
+        }
         this.dm_maxfilecount = dm_maxfilecount;
+    }
+
+    public DBConfig(){
+        this.dbpath = "./DB/binData";
+        this.pageSize = 1;
+        this.dm_maxfilecount = 1;
     }
 
     public String getDbpath() {
@@ -27,7 +44,6 @@ public class DBConfig {
     }
 
     public static DBConfig loadDBConfig(String fichierConfig) {
-        /* Charge les élément d'un fichier config*/
         Properties props = new Properties();
         DBConfig config = null;
         try {
