8d73ac0daf79c03722a6253a5f90ca437109e2ec
Anne-Louis <annelouis.vojinovic@gmail.com>
2025-10-14 09:13:19 +0200

    Séparation InfoColonne et Relation

--- src/bdda/config/Relation.java
+++ src/bdda/config/Relation.java
@@ -3,56 +3,6 @@ package bdda.config;
 import java.util.List;
 
 /**
- * Tuple composé de <nom, type> pour chaque colonne d'une relation
- */
-class InfoColonne<nom extends CharSequence, type extends CharSequence>{
-
-    /** Nom de la colonne */
-    private String nom;
-
-    /** Type de la colonne entre INT, FLOAT, CHAR et VARCHAR */
-    private String type;
-
-    /** Crée un tuple InfoColonne */
-    public InfoColonne(){
-        this.nom = new String();
-        this.type = new String();
-    }
-
-    /**
-     * Récupère la nom
-     * @return le nom
-     */
-    public String getNom() {
-        return nom;
-    }
-
-    /**
-     * Récupère
-     * @return le type
-     */
-    public String getType() {
-        return type;
-    }
-
-    /**
-     * Défini le nom
-     * @param nom
-     */
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    /**
-     * Défini le type
-     * @param type
-     */
-    public void setType(String type) {
-        this.type = type;
-    }
-}
-
-/**
  * Classe correspondant à la relation entre une entité et une association"
  */
 public class Relation {
