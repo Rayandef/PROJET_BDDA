bc36eb335c44c2bf9ec224ab629671f5410d69ae
Anne-Louis <annelouis.vojinovic@gmail.com>
2025-10-15 18:17:11 +0200

    Ajout WriteRecordToBuffer et ajout d'enum pour taille des types

--- src/bdda/config/Relation.java
+++ src/bdda/config/Relation.java
@@ -4,10 +4,6 @@ import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
-//INT 4 OCTETS
-//FLOAT 4 OCTETS
-//CHAR 64
-//VARCHAR 64
 
 /**
  * Classe correspondant à la relation entre une entité et une association"
@@ -22,6 +18,21 @@ public class Relation {
 
     /** Info de chaque colonne */
     private List<InfoColonne<String, String>> infoColonne;
+
+    /** Liste des tailles des 4 types de colonnes */
+    public enum Size{
+        INT(4), FLOAT(4), CHAR(64), VARCHAR(64) ;
+
+        private final int taille ;
+
+        private Size(int taille){
+            this.taille = taille ;
+        }
+
+        public int getTaille(){
+            return this.taille ;
+        }
+    }
 
     /** Crée une relation 
      * @param nom
@@ -92,6 +103,66 @@ public class Relation {
         this.nom = nom;
     }
 
+
+    /**
+     * Ecrit les données d'un Record et les stocke dans un ByteBuffer
+     * @param record Le Record contenant les données
+     * @param buffer Le ByteBuffer où les données seront stockées
+     * @param pos La position de départ dans le ByteBuffer
+     */
+    public void writeRecordToBuffer(Record record, ByteBuffer buffer, int pos){
+        buffer.position(pos);
+        List<String> valeurs = record.getValeurs();
+
+        for (int i = 0; i < infoColonne.size(); i++) {
+            InfoColonne<String, String> col = infoColonne.get(i);
+            String type = col.getType().toUpperCase();
+            String valeur = valeurs.get(i);
+
+            switch (type) {
+                case "INT":
+                    int intValue = Integer.parseInt(valeur);
+                    buffer.putInt(intValue);
+                    break;
+
+                case "FLOAT":
+                    float floatValue = Float.parseFloat(valeur);
+                    buffer.putFloat(floatValue);
+                    break;
+
+                case "CHAR":
+                    int length = Size.CHAR.getTaille() ;
+                    if (valeur.length() > length) {
+                        valeur = valeur.substring(0, length);
+                    } else if (valeur.length() < length) {
+                        StringBuilder sb = new StringBuilder(valeur);
+                        while (sb.length() < length) sb.append(' ');
+                        valeur = sb.toString();
+                    }
+
+                    // Écriture caractère par caractère
+                    byte[] bytes = valeur.getBytes(StandardCharsets.UTF_8);
+                    buffer.put(bytes);
+                    break;
+
+                case "VARCHAR":
+                    int maxLength = Size.VARCHAR.getTaille() ;
+                    if (valeur.length() > maxLength) {
+                        valeur = valeur.substring(0, maxLength);
+                    }
+
+                    byte[] data = valeur.getBytes(StandardCharsets.UTF_8);
+                    buffer.putInt(data.length);  // longueur réelle
+                    buffer.put(data);            // contenu
+                    break;
+
+                default:
+                    System.err.println("Type inconnu : " + type);
+                    break;
+            }
+        }
+    }
+
     /**
      * Lit les données d'un ByteBuffer et les stocke dans un Record
      * @param record Le Record où les données seront stockées
@@ -112,13 +183,13 @@ public class Relation {
                 float val = buffer.getFloat();
                 valeurs.add(String.valueOf(val));
             } else if (type.equals("CHAR")) {
-                int size = 64; 
+                int size = Size.CHAR.getTaille(); 
                 byte[] data = new byte[size];
                 buffer.get(data);
                 String str = new String(data, StandardCharsets.UTF_8).trim();
                 valeurs.add(str);
             } else if (type.equals("VARCHAR")) {
-                int maxSize = 64; 
+                int maxSize = Size.VARCHAR.getTaille(); 
                 int len = buffer.getInt(); 
                 if (len > maxSize) len = maxSize;
                 byte[] data = new byte[len];
@@ -129,5 +200,5 @@ public class Relation {
         }
 
     record.setValeurs(valeurs);
-}
+    }
 }
