c391cd919154778bfc94c462a8a0d2eba9e378c9
RonanLall <ronanlall@gmail.com>
2025-10-15 14:43:10 +0200

    Ajout du readFromBuffer

--- src/bdda/config/Relation.java
+++ src/bdda/config/Relation.java
@@ -1,5 +1,8 @@
 package bdda.config;
 
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 //INT 4 OCTETS
 //FLOAT 4 OCTETS
@@ -27,9 +30,9 @@ public class Relation {
     public Relation(String nom, List<InfoColonne<String, String>> infoColonne) throws Exception{
         this.nom = nom;
         for(InfoColonne<String, String> info : infoColonne){
-            if(info.getType() != "INT" || info.getType() != "FLOAT" || info.getType() != "CHAR" || info.getType() != "VARCHAR"){
-                throw new Exception("L'élément " + (infoColonne.indexOf(info) + 1) + "de la liste est incorrecte");
-            }
+            if (!(info.getType().equals("INT") ||info.getType().equals("FLOAT") || info.getType().equals("CHAR") || info.getType().equals("VARCHAR"))) {
+                throw new Exception("L'élément " + (infoColonne.indexOf(info) + 1) + " de la liste est incorrect");
+            } 
             this.infoColonne = infoColonne;
         }
         colonne = infoColonne.size();
@@ -89,5 +92,42 @@ public class Relation {
         this.nom = nom;
     }
 
+    /**
+     * Lit les données d'un ByteBuffer et les stocke dans un Record
+     * @param record Le Record où les données seront stockées
+     * @param buffer Le ByteBuffer contenant les données
+     * @param pos La position de départ dans le ByteBuffer
+     */
+    public void readFromBuffer(Record record, ByteBuffer buffer, int pos) {
+        buffer.position(pos);
+        List<String> valeurs = new ArrayList<>();
 
+        for (InfoColonne<String, String> info : infoColonne) {
+            String type = info.getType();
+
+            if (type.equals("INT")) {
+                int val = buffer.getInt();
+                valeurs.add(String.valueOf(val));
+            } else if (type.equals("FLOAT")) {
+                float val = buffer.getFloat();
+                valeurs.add(String.valueOf(val));
+            } else if (type.equals("CHAR")) {
+                int size = 64; 
+                byte[] data = new byte[size];
+                buffer.get(data);
+                String str = new String(data, StandardCharsets.UTF_8).trim();
+                valeurs.add(str);
+            } else if (type.equals("VARCHAR")) {
+                int maxSize = 64; 
+                int len = buffer.getInt(); 
+                if (len > maxSize) len = maxSize;
+                byte[] data = new byte[len];
+                buffer.get(data);
+                String str = new String(data, StandardCharsets.UTF_8);
+                valeurs.add(str);
+            }
+        }
+
+    record.setValeurs(valeurs);
+}
 }
