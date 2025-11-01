ca3205f6f4da75b9bad83d2bf98169139037a9b9
SlurXR <mouakkilirayanperso@gmail.com>
2025-11-01 12:13:01 +0100

    Modification classe RelationTests

--- src/bdda/test/RelationTests.java
+++ src/bdda/test/RelationTests.java
@@ -1,88 +1,68 @@
 package bdda.test;
 
 import java.nio.ByteBuffer;
-import java.util.*;
-import bdda.config.*;
+import java.util.Arrays;
+import java.util.List;
+
+import bdda.config.InfoColonne;
+import bdda.config.Relation;
+import bdda.config.Record;
 
 /**
- * Classe de test pour la classe Relation.
- * Teste la lecture et l'écriture de Record dans un ByteBuffer.
+ * Classe de test pour Relation
  * @author Rayan
  * @version 1.0
  */
 public class RelationTests {
 
-    public static void testIntFloat() throws Exception {
-        System.out.println("Test Relation : INT et FLOAT");
-
-        // Définition des colonnes
-        InfoColonne<String, String> c1 = new InfoColonne<>();
-        c1.setNom("id");
-        c1.setType("INT");
-
-        InfoColonne<String, String> c2 = new InfoColonne<>();
-        c2.setNom("note");
-        c2.setType("FLOAT");
-
-        List<InfoColonne<String, String>> colonnes = Arrays.asList(c1, c2);
-        Relation rel = new Relation("Notes", colonnes);
-
-        // Création d’un enregistrement
-        Record record = new Record(Arrays.asList("42", "18.5"));
-
-        // Création d’un buffer et écriture du record
-        ByteBuffer buffer = ByteBuffer.allocate(100);
-        rel.writeRecordToBuffer(record, buffer, 0);
-
-        // Lecture dans un nouveau record
-        Record recordLu = new Record();
-        rel.readFromBuffer(recordLu, buffer, 0);
-
-        System.out.println("Record initial : " + record);
-        System.out.println("Record lu      : " + recordLu);
-        System.out.println("Fin Test INT et FLOAT\n");
-    }
-
-    public static void testCharVarchar() throws Exception {
-        System.out.println("Test Relation : CHAR et VARCHAR");
-
-        // Définition des colonnes
-        InfoColonne<String, String> c1 = new InfoColonne<>();
-        c1.setNom("prenom");
-        c1.setType("CHAR");
-
-        InfoColonne<String, String> c2 = new InfoColonne<>();
-        c2.setNom("ville");
-        c2.setType("VARCHAR");
-
-        List<InfoColonne<String, String>> colonnes = Arrays.asList(c1, c2);
-        Relation rel = new Relation("Personnes", colonnes);
-
-        // Création d’un enregistrement
-        Record record = new Record(Arrays.asList("Rayan", "Paris"));
-
-        // Création d’un buffer et écriture du record
-        ByteBuffer buffer = ByteBuffer.allocate(200);
-        rel.writeRecordToBuffer(record, buffer, 0);
-
-        // Lecture dans un nouveau record
-        Record recordLu = new Record();
-        rel.readFromBuffer(recordLu, buffer, 0);
-
-        System.out.println("Record initial : " + record);
-        System.out.println("Record lu      : " + recordLu);
-        System.out.println("Fin Test CHAR et VARCHAR\n");
-    }
-
+    
     public static void main(String[] args) {
+        System.out.println("TESTS Relation");
+
         try {
-            System.out.println("Début des tests Relation\n");
-            testIntFloat();
-            testCharVarchar();
-            System.out.println("Fin des tests Relation");
+            // === Création des colonnes ===
+            InfoColonne<String, String> col1 = new InfoColonne<>();
+            col1.setNom("id");
+            col1.setType("INT");
+
+            InfoColonne<String, String> col2 = new InfoColonne<>();
+            col2.setNom("note");
+            col2.setType("FLOAT");
+
+            InfoColonne<String, String> col3 = new InfoColonne<>();
+            col3.setNom("nom");
+            col3.setType("CHAR");
+
+            InfoColonne<String, String> col4 = new InfoColonne<>();
+            col4.setNom("ville");
+            col4.setType("VARCHAR");
+
+            List<InfoColonne<String, String>> colonnes = Arrays.asList(col1, col2, col3, col4);
+
+            Relation relation = new Relation("Etudiants", colonnes);
+            System.out.println("Relation créée avec succès : " + relation.getNom() + " (" + relation.getColonne() + " colonnes)");
+
+            bdda.config.Record record = new bdda.config.Record(Arrays.asList(
+                "42", "18.5", "Rayan", "Paris"
+            ));
+
+            int bufferSize = 200;
+            ByteBuffer buffer = ByteBuffer.allocate(bufferSize);
+
+            relation.writeRecordToBuffer(record, buffer, 0);
+            System.out.println("Écriture du record dans le buffer réussie.");
+
+            bdda.config.Record recordLu = new bdda.config.Record();
+            relation.readFromBuffer(recordLu, buffer, 0);
+
+            System.out.println("Lecture du buffer réussie !");
+            System.out.println("Valeurs lues : " + recordLu.getValeurs());
+
         } catch (Exception e) {
-            System.out.println("Erreur pendant les tests Relation : " + e.getMessage());
+            System.out.println("Erreur pendant le test Relation : " + e.getMessage());
             e.printStackTrace();
         }
+
+        System.out.println("FIN TEST Relation");
     }
 }
