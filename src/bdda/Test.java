e3b08d36f7fb3164561300e43baf8ead0eeb83af
Anne-Louis <annelouis.vojinovic@gmail.com>
2025-11-27 17:29:06 +0100

    Correction après débug de Relation

--- src/bdda/Test.java
+++ src/bdda/Test.java
@@ -3,12 +3,14 @@ package bdda;
 import bdda.test.TestDBConfig;
 import bdda.test.DiskManagerTests;
 import bdda.test.BufferManagerTests;
+import bdda.test.RelationTests;
 
 public class Test {
     public static void main(String[] args){
         TestDBConfig.runTests();
         DiskManagerTests.main(args);
         BufferManagerTests.main(args);
+        RelationTests.main(args);
         System.out.println("FIN");
     }
 }
